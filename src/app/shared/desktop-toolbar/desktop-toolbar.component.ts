b8f930d8b588f56bdbf95955f34aa9d3e4c77093
Ken Sodemann <ken@ionic.io>
2020-11-11 14:18:03 -0600

    feat(app): move between mobile and desktop

--- src/app/shared/desktop-toolbar/desktop-toolbar.component.ts
+++ src/app/shared/desktop-toolbar/desktop-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-desktop-toolbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desktop-toolbar.component.scss'],
 })
 export class DesktopToolbarComponent implements OnInit {
+  @Input() username: string;
+
   constructor() {}
 
   ngOnInit() {}
