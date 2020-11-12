8795348dcb770867dfffc8db6509765faa0de858
Ken Sodemann <ken@ionic.io>
2020-11-12 06:03:40 -0600

    fix(app): update the tests to match changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,8 +28,8 @@ describe('AppComponent', () => {
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
     expect(menuItems.length).toEqual(6);
-    expect(menuItems[0].textContent).toContain('Inbox');
-    expect(menuItems[1].textContent).toContain('Outbox');
+    expect(menuItems[0].textContent).toContain('Home');
+    expect(menuItems[1].textContent).toContain('New Claim');
   });
 
   it('should have urls', () => {
@@ -39,10 +39,10 @@ describe('AppComponent', () => {
     const menuItems = app.querySelectorAll('ion-item');
     expect(menuItems.length).toEqual(6);
     expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual(
-      '/folder/Inbox',
+      '/folder/home',
     );
     expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual(
-      '/folder/Outbox',
+      '/folder/claim',
     );
   });
 });
